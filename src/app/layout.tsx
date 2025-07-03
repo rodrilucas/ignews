91b861a74ad412d728e81e88befcdf7670694ee0
rodrilucas <lucas.celery@gmail.com>
2025-07-03 17:45:43 -0300

    fix: remove roboto mono

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
-import { Roboto, Roboto_Mono } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "@/styles/global.scss";
 
 const roboto = Roboto({
   variable: "--font-roboto",
-  display: "swap",
-  subsets: ["latin"],
-});
-
-const robotoMono = Roboto_Mono({
-  variable: "--font-roboto-mono",
   display: "swap",
   subsets: ["latin"],
 });
@@ -23,9 +17,7 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="/favicon.png" type="image/png" />
       </head>
-      <body className={`${roboto.className} ${robotoMono.className}`}>
-        {children}
-      </body>
+      <body className={`${roboto.className}`}>{children}</body>
     </html>
   );
 }
