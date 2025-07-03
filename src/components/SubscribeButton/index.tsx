577c7130641d3fae4f1a0e8ef4addf1af47f10b3
rodrilucas <lucas.celery@gmail.com>
2025-07-03 18:00:41 -0300

    feat: subscribe button component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  return (
+    <button className={styles.subscribe} type="button">
+      Subscribe Now
+    </button>
+  );
+}
