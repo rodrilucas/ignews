e655b1b3a51b708e60743c8fd1f9379368438368
rodrilucas <lucas.celery@gmail.com>
2025-07-09 09:55:33 -0300

    feat: add sign in and sign out events

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,25 @@
+"use client";
+
 import { FaGithub } from "react-icons/fa6";
 import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.button} type="button">
+  return session ? (
+    <button className={styles.button} type="button" onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Lucas Rodrigues
+      {session.user?.name}
       <FiX color="#737380" className={styles.close} />
     </button>
   ) : (
-    <button className={styles.button} type="button">
+    <button
+      className={styles.button}
+      type="button"
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
