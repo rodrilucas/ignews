5db743bd47a0a558af926dc6fb026b2a3f109181
rodrilucas <lucas.celery@gmail.com>
2025-07-10 09:38:38 -0300

    feat: add event to subscribe button and redirect to payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,8 +1,43 @@
-import styles from "./styles.module.scss";
+"use client";
 
-export function SubscribeButton() {
+import { signIn, useSession } from "next-auth/react";
+import styles from "./styles.module.scss";
+import { api } from "@/services/api";
+import { getStripeJs } from "@/services/stripe/stripe-js";
+
+type SubscribeButtonProps = {
+  priceId: string;
+};
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session, status } = useSession();
+
+  async function handleSubscribe() {
+    if (status !== "authenticated") {
+      signIn("github");
+      return;
+    }
+
+    try {
+      const response = await api();
+      const { sessionId } = response;
+
+      const stripe = await getStripeJs();
+
+      if (!stripe) throw new Error("Stripe.js não carregado");
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      throw error;
+    }
+  }
+
   return (
-    <button className={styles.subscribe} type="button">
+    <button
+      className={styles.subscribe}
+      type="button"
+      onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   );
