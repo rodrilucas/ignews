30dcdbc6c78959af41f9e08c6e060cf3dc851e1e
rodrilucas <lucas.celery@gmail.com>
2025-07-10 09:25:15 -0300

    fix: import the server environment variable

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 import { version } from "../../package.json";
-import { env } from "@/env";
+import { env } from "@/env/env.server";
 
 export const stripe = new Stripe(env.STRIPE_API_KEY, {
   apiVersion: "2025-06-30.basil",
