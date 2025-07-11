01a92196e1357b2b1891b771071cc969b751d28c
rodrilucas <lucas.celery@gmail.com>
2025-07-11 19:45:02 -0300

    feat: add new methods to the repository and make the types more generic

--- src/repositories/firebase/index.ts
+++ src/repositories/firebase/index.ts
@@ -1,16 +1,35 @@
-export type FindManyData<T> = {
-  data: {
-    field: keyof T;
-    value: string;
-  };
+export interface Id {
+  id: string;
+}
+
+export type FindUnique<T> = {
+  field: keyof T;
+  value: T[keyof T];
 };
 
-export type UpdateData<T extends { id: string }> = {
-  data: T;
+export type FindMany<T> = {
+  filters: Partial<T>;
 };
 
-export interface IFirebaseRepository<T extends { id: string }> {
+export type UpdateOne<T> = {
+  id: string;
+  data: Partial<T>;
+};
+
+export type UpdateMany<T> = {
+  filters: Partial<T>;
+  data: Partial<T>;
+};
+
+export type FindUserWithActiveSubscription<T> = FindUnique<T>;
+
+export interface IFirebaseRepository<T extends Id> {
   create(data: Omit<T, "id">): Promise<void>;
-  findUnique(data: FindManyData<T>): Promise<T | null>;
-  update(data: UpdateData<T>): Promise<void>;
+  findUnique(params: FindUnique<T>): Promise<T | null>;
+  findMany(params: FindMany<T>): Promise<T[]>; 
+  updateOne(params: UpdateOne<T>): Promise<boolean>;
+  updateMany(params: UpdateMany<T>): Promise<boolean>;
+  findUserWithActiveSubscription(
+    params: FindUserWithActiveSubscription<T>
+  ): Promise<T | null>;
 }
--- src/repositories/firebase/repository.ts
+++ src/repositories/firebase/repository.ts
@@ -1,22 +1,33 @@
 import {
-  addDoc,
   collection,
   query,
-  getDocs,
   where,
+  getDocs,
+  addDoc,
   updateDoc,
   doc,
+  writeBatch,
   Firestore,
+  CollectionReference,
 } from "firebase/firestore/lite";
-
-import type { IFirebaseRepository, FindManyData, UpdateData } from ".";
+import type {
+  IFirebaseRepository,
+  FindUnique,
+  FindMany,
+  UpdateOne,
+  UpdateMany,
+  FindUserWithActiveSubscription,
+} from ".";
 
 export class FirebaseRepository<T extends { id: string }>
   implements IFirebaseRepository<T>
 {
-  private readonly collectionRef;
+  private readonly collectionRef: CollectionReference;
 
-  constructor(private readonly db: Firestore, collectionName: string) {
+  constructor(
+    private readonly db: Firestore,
+    collectionName: string
+  ) {
     this.collectionRef = collection(db, collectionName);
   }
 
@@ -24,22 +35,84 @@ export class FirebaseRepository<T extends { id: string }>
     await addDoc(this.collectionRef, data);
   }
 
-  async findUnique({ data }: FindManyData<T>): Promise<T | null> {
-    const q = query(
-      this.collectionRef,
-      where(data.field as string, "==", data.value)
-    );
+  async findUnique({ field, value }: FindUnique<T>): Promise<T | null> {
+    const q = query(this.collectionRef, where(field as string, "==", value));
     const snap = await getDocs(q);
-
     if (snap.empty) return null;
-
     const docSnap = snap.docs[0];
     return { id: docSnap.id, ...docSnap.data() } as T;
   }
 
-  async update({ data }: UpdateData<T>): Promise<void> {
-    const docRef = doc(this.collectionRef, data.id);
-    const { id, ...rest } = data;
+  async findMany({ filters }: FindMany<T>): Promise<T[]> {
+    const constraints = Object.entries(filters).map(([field, value]) =>
+      where(field, "==", value)
+    );
+
+    const q = query(this.collectionRef, ...constraints);
+    const snap = await getDocs(q);
+    return snap.docs.map((docSnap) => ({
+      id: docSnap.id,
+      ...docSnap.data(),
+    })) as T[];
+  }
+
+  async updateOne({ id, data }: UpdateOne<T>): Promise<boolean> {
+    const docRef = doc(this.collectionRef, id);
+    const { id: _, ...rest } = data;
     await updateDoc(docRef, rest);
+    return true;
+  }
+
+  async updateMany({ filters, data }: UpdateMany<T>): Promise<boolean> {
+    const constraints = Object.entries(filters).map(([field, value]) =>
+      where(field, "==", value)
+    );
+
+    const q = query(this.collectionRef, ...constraints);
+    const snap = await getDocs(q);
+    if (snap.empty) return false;
+
+    const batch = writeBatch(this.db);
+    const safeData = Object.fromEntries(
+      Object.entries(data).filter(([_, v]) => v !== undefined)
+    );
+
+    snap.forEach((docSnap) => {
+      const ref = doc(this.db, this.collectionRef.path, docSnap.id);
+      batch.update(ref, safeData);
+    });
+
+    await batch.commit();
+
+    return true;
+  }
+
+  async findUserWithActiveSubscription({
+    field,
+    value,
+  }: FindUserWithActiveSubscription<T>) {
+    const usersRef = collection(this.db, "users");
+    const userQuery = query(usersRef, where(field as string, "==", value));
+    const userSnap = await getDocs(userQuery);
+    const getByIndex = 0;
+
+    if (userSnap.empty) return null;
+
+    const userDoc = userSnap.docs[getByIndex];
+    const userData = userDoc.data() as { stripe_customer_id: string };
+
+    const subsRef = collection(this.db, "subscriptions");
+    const subQuery = query(
+      subsRef,
+      where("user_id", "==", userData.stripe_customer_id),
+      where("status", "==", "active")
+    );
+    const subSnap = await getDocs(subQuery);
+
+    if (subSnap.empty) return null;
+
+    const subscriptionDoc = subSnap.docs[getByIndex];
+
+    return { ...subscriptionDoc.data() } as T;
   }
 }
