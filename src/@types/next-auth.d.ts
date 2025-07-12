5787252876d96f630cff687c473615b9bf16206e
rodrilucas <lucas.celery@gmail.com>
2025-07-12 12:19:50 -0300

    feat: session and jwt types

---
+++ src/@types/next-auth.d.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import type { JWT as DefaultJWT } from "next-auth/jwt";
+import type { User } from "next-auth";
+import type { Subscription } from "@/services/factories/make-firebase-subscription-repository";
+
+declare module "next-auth" {
+  interface Session extends User {
+    subscription: Subscription | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    subscription: Subscription | null;
+  }
+}
