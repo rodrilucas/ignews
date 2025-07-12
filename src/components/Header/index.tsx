ac0e48260ec0bee64ccabaaf981419917cea375a
rodrilucas <lucas.celery@gmail.com>
2025-07-12 12:08:02 -0300

    feat: add link component in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,24 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import SignInButton from "../SignInButton";
+import { randomUUID } from "crypto";
+import { ActiveLink } from "../ActiveLink";
+
+const links = [
+  { to: "/", title: "Home" },
+  { to: "/posts", title: "Posts" },
+];
 
 export default function Header() {
+  const renderedLinks = links.map((link) => {
+    const id = randomUUID();
+    return (
+      <ActiveLink key={id} to={link.to}>
+        {link.title}
+      </ActiveLink>
+    );
+  });
+
   return (
     <header className={styles.header}>
       <div className={styles.content}>
@@ -12,10 +28,7 @@ export default function Header() {
           width={100}
           height={100}
         />
-        <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
-        </nav>
+        <nav>{renderedLinks}</nav>
         <SignInButton />
       </div>
     </header>
