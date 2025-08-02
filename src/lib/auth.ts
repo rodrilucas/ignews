c815ac85cc691f14c8d562f8d635d911ac890874
rodrilucas <lucas.celery@gmail.com>
2025-08-02 13:43:34 -0300

    fix: create user and search for active subscription

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -20,39 +20,50 @@ export const authOptions: NextAuthOptions = {
   secret: env.AUTH_SECRET,
   callbacks: {
     async session({ session }) {
+      if (!session.user || !session.user.email) {
+        return session;
+      }
+
       const userFirebaseRepository = makeFirebaseUserRepository();
 
-      if (!session.user || !session.user.email) {
-        throw new Error("Usuário não autenticado ou email não encontrado..");
-      }
-
       const subscription =
-        await userFirebaseRepository.findUserWithActiveSubscription({
+        (await userFirebaseRepository.findUserWithActiveSubscription({
           field: "email",
           value: session.user.email,
-        }) as Subscription;
+        })) as Subscription;
 
       if (subscription) {
-        session.subscription = {
-          ...subscription,
-        };
+        session.subscription = { ...subscription };
       }
+
       return session;
     },
+
     async signIn({ user }) {
       if (!user || !user.email) {
         throw new Error("Usuário não existe ou email não encontrado.");
       }
 
+      const userFirebaseRepository = makeFirebaseUserRepository();
+
       try {
-        await add({
-          data: {
-            email: user.email,
-          },
+        const existingUser = await userFirebaseRepository.findUnique({
+          field: "email",
+          value: user.email,
         });
+
+        if (!existingUser) {
+          await add({
+            data: {
+              email: user.email,
+            },
+          });
+        }
+
         return true;
       } catch (err) {
-        throw err;
+        console.error("Erro ao verificar ou adicionar usuário:", err);
+        return false;
       }
     },
   },
